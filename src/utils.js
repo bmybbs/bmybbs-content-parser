43e0770414615fc0c5cc167fdbfe2e3272cff3f0
IronBlood <aplshenyang@gmail.com>
2021-01-18 16:08:18 +0800

    新增一些工具方法

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,31 @@
+const escapeHtml = function(unsafe) {
+	return unsafe
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;");
+};
+
 const isObject = function(o) {
-	return (o != null && typeof object === "object");
-}
+	return ((o != null) && (typeof o === "object"));
+};
 
 const isText = function(s) {
 	return (typeof s === "string" || s instanceof String);
-}
+};
 
-export { isObject, isText };
+// TODO ref: https://prismjs.com/#supported-languages
+const langFallback = "markup";
+const langSets = new Set();
+langSets.add("markup");
+langSets.add("html");
+
+export {
+	escapeHtml,
+	isObject,
+	isText,
+	langSets,
+	langFallback,
+};
 
